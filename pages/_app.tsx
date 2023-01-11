bc344df5342704db85e4e1f3dfe2ceccf93b27ce
Fabian Kelvans <fkelvans@gmail.com>
2023-01-11 11:44:45 +0300

    update-build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,36 @@ import type { AppProps } from 'next/app'
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import {useEffect, useRef} from "react";
+import Lenis from "@studio-freight/lenis";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 2.6,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+            direction: 'vertical', // vertical, horizontal
+            gestureDirection: 'vertical', // vertical, horizontal, both
+            smooth: true,
+            mouseMultiplier: 1,
+            smoothTouch: true,
+            touchMultiplier: 2,
+            infinite: false,
+        })
+
+//get scroll value
+        {/*@ts-ignore*/}
+        lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
+            console.log({ scroll, limit, velocity, direction, progress })
+        })
+
+        function raf(time:any) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+
+        requestAnimationFrame(raf)
+    })
 
   return (
       <>
